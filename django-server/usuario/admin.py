cbeb35d421530fbe92ee3e514eb5b69152fe4cb5
DGCORruie23 <dgcor.rui23@gmail.com>
2024-08-23 21:24:32 -0600

    archivos iniciales

---
+++ django-server/usuario/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Usuario, Paises, EstadoFuerza, Frases, Municipios, PuntosInternacion, RescatePunto, ConteoRapidoPunto, MsgUpdate
+
+admin.site.register(Usuario)
+admin.site.register(Paises)
+admin.site.register(EstadoFuerza)
+admin.site.register(Frases)
+admin.site.register(Municipios)
+admin.site.register(PuntosInternacion)
+admin.site.register(RescatePunto)
+admin.site.register(ConteoRapidoPunto)
+admin.site.register(MsgUpdate)
